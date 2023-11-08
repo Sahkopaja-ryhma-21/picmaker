ff0422e6c1e9b083c87938093af25210a9a50b28
Jere Hasu <31950088+zexee24@users.noreply.github.com>
2023-11-08 13:59:07 +0200

    add: Args and ablity to find paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::read_to_string, error::Error};
+use args::Args;
+use clap::Parser;
+
+mod args;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args = Args::parse();
+    let file_contents = read_to_string(args.filename)?;
+    let doc = roxmltree::Document::parse(&file_contents)?;
+    let paths = doc.descendants().filter(|x| x.tag_name().name()== "path").collect::<Vec<_>>();
+
+    let position_vec = paths.iter().map(|x| x.attribute("d")).collect::<Vec<_>>();
+
+    println!("{:?}", position_vec);
+
+    Ok(())
 }
+
